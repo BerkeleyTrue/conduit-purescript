ecaa5d363c3866673b97f191108289d474db5dfd
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-02-27 20:23:10 -0700

    feat: refactor out shutdown handler, add uncaughtExeption

---
+++ src/Server/Infra/HttPure/GracefullShutdown.purs
@@ -0,0 +1,22 @@
+module Server.Infra.HTTPurple.GracefullShutdown where
+
+import Prelude
+
+import Data.Posix.Signal (Signal(..))
+import Effect (Effect)
+import Effect.Console (log)
+import Node.Process (onSignal, onUncaughtException)
+
+gracefullShutdown :: (Effect Unit -> Effect Unit) -> Effect Unit
+gracefullShutdown shutdown = do
+  let
+    shutdownHandler = do
+      log "Shutting down server..."
+      shutdown $ log "Server shutdown"
+
+  onSignal SIGINT shutdownHandler
+  onSignal SIGTERM shutdownHandler
+  onSignal SIGUSR2 shutdownHandler -- nodemon
+  onUncaughtException \err -> do
+    _ <- log $ "Uncaught exception: " <> show err
+    shutdownHandler
--- src/Server/Main.purs
+++ src/Server/Main.purs
@@ -3,14 +3,11 @@ module Server.Main (main) where
 import Prelude hiding ((/))
 
 import Data.Generic.Rep (class Generic)
-import Data.Posix.Signal (Signal(..))
 import Effect (Effect)
-import Effect.Aff (launchAff_)
-import Effect.Class (liftEffect)
 import Effect.Console (log)
 import HTTPurple (Method(..), Request, ResponseM, RouteDuplex', catchAll, mkRoute, noArgs, notFound, ok, response, serve, (/))
 import HTTPurple.Status as Status
-import Node.Process (onSignal)
+import Server.Infra.HTTPurple.GracefullShutdown (gracefullShutdown)
 import Server.Infra.HttPurple.Middleware.Logger (developmentLogFormat)
 
 data Route
@@ -28,7 +25,7 @@ route = mkRoute
   }
 
 router :: Request Route -> ResponseM
-router { route: Home, method: Get } = ok "hello foo!"
+router { route: Home, method: Get } = ok "hello world!"
 router { route: Ping, method: Get } = ok "pong"
 router { route: CatchAll _ } = response Status.notFound "Opps, something went wrong!"
 router _ = notFound
@@ -37,17 +34,8 @@ port :: Int
 port = 3000
 
 main :: Effect Unit
-main = do
-  liftEffect do
-    shutdown <- serve { port, onStarted } { route, router: developmentLogFormat router }
-
-    let shutdownHandler = launchAff_ do
-          liftEffect $ log "Shutting down server..."
-          liftEffect $ shutdown $ log "Server shutdown"
-
-    onSignal SIGINT shutdownHandler
-    onSignal SIGTERM shutdownHandler
-    onSignal SIGUSR2 shutdownHandler -- nodemon
-
+main = serve opts settings >>= gracefullShutdown
   where
   onStarted = log $ "Server started on port " <> show port
+  opts = { port, onStarted }
+  settings = { route, router: developmentLogFormat router }
