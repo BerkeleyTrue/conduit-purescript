dfa83cbc4432d7dd37a09dd61b4c6f118eb03ace
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-03-03 22:56:08 -0700

    feat: add custom not found handler

--- src/Server/Infra/HttPurple/HttPurple.purs
+++ src/Server/Infra/HttPurple/HttPurple.purs
@@ -2,11 +2,17 @@ module Server.Infra.HttPurple (createServer) where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
-import HTTPurple (Request, ResponseM, RouteDuplex', serve)
+import HTTPurple (Request, ResponseM, RouteDuplex', response, serve)
+import HTTPurple.Status as Status
 import Server.Infra.HttPurple.Middleware.Logger (developmentLogFormat)
 import Server.Infra.Node.GracefullShutdown (gracefullShutdown)
+
+
+notFoundHandler :: Request Unit -> ResponseM
+notFoundHandler = const $ response Status.notFound "Oops, something went wrong"
 
 createServer
   :: forall route
@@ -17,5 +23,5 @@ createServer
 createServer route router port = serve opts settings >>= gracefullShutdown
   where
   onStarted = log $ "Server started on port " <> show port
-  opts = { port, onStarted }
+  opts = { port, onStarted, notFoundHandler: Just notFoundHandler }
   settings = { route, router: developmentLogFormat router }
