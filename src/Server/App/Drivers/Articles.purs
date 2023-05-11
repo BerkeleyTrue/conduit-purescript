5459e14213c17c824beb82486e27e8f62e458a8b
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-05-11 17:18:02 -0600

    feat(ArticlesRouter): add update article

--- src/Server/App/Drivers/Articles.purs
+++ src/Server/App/Drivers/Articles.purs
@@ -7,20 +7,22 @@ module Server.App.Drivers.Articles
 import Prelude hiding ((/))
 
 import Conduit.Data.Limit (Limit(..))
-import Conduit.Data.MySlug (MySlug, toString)
+import Conduit.Data.MySlug (MySlug)
+import Conduit.Data.MySlug as Slug
 import Conduit.Data.Offset (Offset(..))
 import Conduit.Data.UserId (AuthorId)
 import Conduit.Data.Username (Username)
 import Data.Generic.Rep (class Generic)
 import Data.Maybe (Maybe(..), fromMaybe)
-import HTTPurple (Method(..), Response, RouteDuplex', badRequest', forbidden, int, jsonHeaders, notFound, ok, ok', optional, params, prefix, segment, string, sum, (/), (?))
+import Foreign (MultipleErrors)
+import HTTPurple (Method(..), Response, RouteDuplex', badRequest', forbidden, int, jsonHeaders, notFound, ok, ok', optional, params, prefix, segment, string, toString, sum, (/), (?))
 import Justifill (justifill)
-import Server.Core.Services.Articles (ArticleService(..))
+import Server.Core.Services.Articles (ArticleService(..), ArticleUpdateInput)
 import Server.Core.Services.User (UserOutput)
 import Server.Infra.Data.Route (limitR, offsetR, slugR, userIdR)
 import Server.Infra.HttPurple.Types (OmRouter)
-import Yoga.JSON (writeJSON)
-import Yoga.Om (Om, handleErrors)
+import Yoga.JSON (readJSON, writeJSON)
+import Yoga.Om (Om, expandErr, fromAff, handleErrors, throw, throwLeftAsM)
 
 data ArticlesRoute
   = List -- Get
@@ -64,16 +66,17 @@ type ArticlesRouterExts ext = (user :: Maybe UserOutput | ext)
 
 defaultErrorHandlers
   :: forall ctx errOut
-   . Om ctx (articleRepoErr :: String | errOut) Response
+   . Om ctx (articleRepoErr :: String, parsingErr :: MultipleErrors | errOut) Response
   -> Om ctx errOut Response
 defaultErrorHandlers = handleErrors
   { articleRepoErr: \err -> badRequest' jsonHeaders $ writeJSON { message: show err }
+  , parsingErr: \err -> badRequest' jsonHeaders $ writeJSON { message: show err }
   }
 
 mkArticlesRouter :: forall ext. { articleService :: ArticleService } -> OmRouter ArticlesRoute (ArticlesRouterExts ext)
 mkArticlesRouter { articleService: (ArticleService { list }) } { route: List { limit, offset, favorited, author, tag }, method: Get, user } = defaultErrorHandlers do
   let (username :: Maybe Username) = user <#> _.username
-  output <- list { username, input }
+  output <- expandErr $ list { username, input }
   ok' jsonHeaders $ writeJSON output
 
   where
@@ -94,7 +97,7 @@ mkArticlesRouter { articleService: (ArticleService { list }) } { route: Feed { l
   case user <#> _.username of
     Nothing -> forbidden
     Just username -> do
-      output <- list { username: Just username, input }
+      output <- expandErr $ list { username: Just username, input }
       ok' jsonHeaders $ writeJSON output
 
   where
@@ -109,28 +112,36 @@ mkArticlesRouter _ { route: Feed _ } = notFound
 
 -- Get Article by Slug
 mkArticlesRouter { articleService: (ArticleService { getBySlug }) } { route: BySlug slug, method: Get } = defaultErrorHandlers do
-  output <- getBySlug slug
+  output <- expandErr $ getBySlug slug
   ok' jsonHeaders $ writeJSON output
 
 -- Update Article by Slug
-mkArticlesRouter _ { route: BySlug slug, method: Put } = ok $ "Update by Slug: " <> toString slug
+mkArticlesRouter { articleService: (ArticleService { update }) } { route: BySlug slug, method: Put, body } = defaultErrorHandlers do
+  str <- fromAff $ toString body
+  parsed <- expandErr $ parseInputFromString str
+  article <- expandErr $ update slug parsed.article
+  ok' jsonHeaders $ writeJSON article
+  where
+  parseInputFromString :: String -> Om {} (parsingErr :: MultipleErrors) { article :: ArticleUpdateInput }
+  parseInputFromString = throwLeftAsM (\err -> throw { parsingErr: err }) <<< readJSON
+
 -- Delete Article by Slug
-mkArticlesRouter _ { route: BySlug slug, method: Delete } = ok $ "Delete by Slug: " <> toString slug
+mkArticlesRouter _ { route: BySlug slug, method: Delete } = ok $ "Delete by Slug: " <> Slug.toString slug
 
 mkArticlesRouter _ { route: BySlug _ } = notFound
 
 -- get comments for article by slug
-mkArticlesRouter _ { route: Comments slug, method: Get } = ok $ "get comments for" <> toString slug
+mkArticlesRouter _ { route: Comments slug, method: Get } = ok $ "get comments for" <> Slug.toString slug
 -- update comments for article by slug
-mkArticlesRouter _ { route: Comments slug, method: Post } = ok $ "post comments for" <> toString slug
+mkArticlesRouter _ { route: Comments slug, method: Post } = ok $ "post comments for" <> Slug.toString slug
 mkArticlesRouter _ { route: Comments _ } = notFound
 
 -- get comment by id for article by slug
-mkArticlesRouter _ { route: Comment slug id, method: Delete } = ok $ "delete " <> (show id) <> " comment on " <> toString slug
+mkArticlesRouter _ { route: Comment slug id, method: Delete } = ok $ "delete " <> (show id) <> " comment on " <> Slug.toString slug
 mkArticlesRouter _ { route: Comment _ _ } = notFound
 
 -- fav an article by slug
-mkArticlesRouter _ { route: Fav slug, method: Get } = ok $ "fav an article " <> toString slug
+mkArticlesRouter _ { route: Fav slug, method: Get } = ok $ "fav an article " <> Slug.toString slug
 -- unfav an article by slug
-mkArticlesRouter _ { route: Fav slug, method: Delete } = ok $ "unfav an article " <> toString slug
+mkArticlesRouter _ { route: Fav slug, method: Delete } = ok $ "unfav an article " <> Slug.toString slug
 mkArticlesRouter _ { route: Fav _ } = notFound
