decffa69c3dee451dc3477280d5ee193d0d176b9
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-07-05 22:19:48 -0600

    fix(Httpurple/Middleware): remove "token " from auth header

--- src/Server/Infra/HttPurple/Middleware/Jwt.purs
+++ src/Server/Infra/HttPurple/Middleware/Jwt.purs
@@ -4,11 +4,14 @@ module Server.Infra.HttPurple.Middleware.Jwt
 
 import Prelude
 
+import Data.Array (last)
 import Data.Maybe (Maybe(..))
+import Data.String (split)
+import Data.String.Pattern (Pattern(..))
 import Data.Tuple.Nested ((/\))
 import Effect.Aff (message)
 import Effect.Class (liftEffect)
-import Effect.Console (error, log)
+import Effect.Console (error)
 import HTTPurple (RequestR, forbidden', internalServerError', jsonHeaders, lookup)
 import HTTPurple.Middleware (Middleware)
 import Prim.Row (class Nub, class Union)
@@ -39,8 +42,11 @@ mkAuthenticateJwtMiddleware (TokenService { decode }) router request@{ headers }
   }
   case lookup headers "Authorization" of
     Nothing -> do
-      liftEffect $ log "No Authorization header"
       fromAff $ router $ merge request { authed: Nothing :: Maybe JwtPayload, user: Nothing :: Maybe UserOutput }
     Just authHeader -> do
-      (authed /\ user) <- expandErr $ decode authHeader
-      fromAff $ router $ merge request { user: Just user, authed: Just authed }
+      case last $ split (Pattern " ") authHeader of
+        Nothing -> do
+          fromAff $ router $ merge request { authed: Nothing :: Maybe JwtPayload, user: Nothing :: Maybe UserOutput }
+        Just token -> do
+          (authed /\ user) <- expandErr $ decode token
+          fromAff $ router $ merge request { user: Just user, authed: Just authed }
